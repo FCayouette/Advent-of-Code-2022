f125699371ad6f3a963c5327e1d5e0204644ce58
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2022-12-04 22:08:19 -0500

    Day 1 F# solution

---
+++ Day01.fs
@@ -0,0 +1,13 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let data = File.ReadAllLines(argv.[0])
+    let almost, last = Array.fold(fun (elves, t) (s:string)-> match s.Length with 
+                            | 0 -> (Array.append elves [|t|], 0)
+                            | _ -> (elves, t + (System.Int32.Parse s))) ([||], 0) data
+    let elves = Array.sortDescending (Array.append almost [|last|])
+
+    printfn "Part 1: %A" <| elves.[0]
+    printfn "Part 2: %A" <| Array.sum elves.[0..2]
+    0 
