163aa62c4b5801f8065cd4885c2267db0456cb94
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2022-12-04 22:20:36 -0500

    Day 2 F# solution

---
+++ Day02.fs
@@ -0,0 +1,9 @@
+﻿open System.IO
+
+[<EntryPoint>]
+let main argv =
+    let pairs = File.ReadAllLines(argv.[0]) |> Array.map(fun (s:string) -> ((int)(s.[0]) - (int)'A', (int)(s.[2])-(int)'X'))
+    let part1, part2 = Array.fold(fun (t1, t2) (a,b) -> (t1 + b+1+3*((4+b-a)%3), t2 + 3*b+(a+b+2)%3+1)) (0,0) pairs
+    printfn("Part 1: %A") <| part1
+    printfn("Part 2: %A") <| part2
+    0
