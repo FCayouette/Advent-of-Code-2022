c6db0cb05c7524c9f87f92c3686bd73159c2809e
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2022-12-06 11:00:56 -0500

    Optimized Day 6 C++20 solution

--- Day06.cpp
+++ Day06.cpp
@@ -1,15 +1,5 @@
 import std.core;
 
-template <int I>
-constexpr bool HasSame(const std::array<char, I>& data)
-{
-	for (int i = 0; i < I - 1; ++i)
-		for (int j = i + 1; j < I; ++j)
-			if (data[i] == data[j])
-				return true;
-	return false;
-}
-
 int main(int argc, char* argv[])
 {
 	if (argc < 2)
@@ -28,28 +18,22 @@ int main(int argc, char* argv[])
 	std::string line;
 	in >> line;
 
-	std::array<char, 4> start;
-	std::array<char, 14> message;
-
-	for (; part1 < 3; ++part1)
-		message[part1] = start[part1] = line[part1];
-
-	do
+	auto Scan = [&line](int& index, int window)
 	{
-		message[part1 % 14] = start[part1 % 4] = line[part1];
-		++part1;
-	} while (HasSame(start));
+		for (int i = index - 2; i >= index - window; --i)
+			for (int j = i + 1; j < index; ++j)
+				if (line[i] == line[j])
+				{
+					index += i - index + window + 1;
+					return true;
+				}
+		return false;
+	};
 
-	part2 = part1;
-	for (; part2 < 13; ++part2)
-		message[part2] = line[part2];
-
-	do
-	{
-		message[part2 % 14] = line[part2];
-		++part2;
-	} while (HasSame(message));
-
+	part1 = 4;
+	while (Scan(part1, 4)) {}
+	part2 = part1 + 10;
+	while (Scan(part2, 14)) {}
 
 	std::cout << std::format("Part 1: {}\nPart 2: {}\n", part1, part2);
 	return 0;
