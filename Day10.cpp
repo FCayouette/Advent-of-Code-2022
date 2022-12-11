5e625d460c86481f254a117884e3decbade36cdf
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2022-12-10 22:19:44 -0500

    Simplified Day 10 C++20 solution

--- Day10.cpp
+++ Day10.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[])
 {
 	if (argc < 2)
 	{
-		std::cout << "Usage: Day09.exe inputFilename\n";
+		std::cout << "Usage: Day10.exe inputFilename\n";
 		return -1;
 	}
 	std::ifstream in(argv[1], std::ios::in);
@@ -13,41 +13,30 @@ int main(int argc, char* argv[])
 		std::cout << "Could not open inputFilename " << argv[1] << std::endl;
 		return -1;
 	}
-	int part1 = 0;
-	std::string inst;
-	int value, X = 1, count = 0;
+	int part1 = 0, value, X = 1, count = 0;
+	std::string part2, inst;
 	
-	auto Strength = [&part1, &X, &count]()
+	auto Check = [&]()
 	{
 		if (count % 40 == 20)
 			part1 += X * count;
-	};
 
-	auto CRT = [&X, &count]()
-	{
-		std::cout << (std::abs((count % 40) - X) < 2 ? '#' : ' ');
+		part2 += (std::abs((count % 40) - X) < 2 ? '#' : ' ');
 		if (++count % 40 == 0)
-			std::cout << '\n';
+			part2 += '\n';
 	};
-	std::cout << "Part 2:\n";
+	
 	while (in >> inst)
 	{
-		if (inst[0] == 'n')
+		Check();
+		if (inst[0] != 'n')
 		{
-			CRT();
-			Strength();
-		}
-		else
-		{
+			Check();
 			in >> value;
-			CRT();
-			Strength();
-			CRT();
-			Strength();
 			X += value;
 		}
 	}
 
-	std::cout << std::format("Part 1: {}\n", part1);
+	std::cout << std::format("Part 1: {}\nPart 2:\n{}", part1, part2);
 	return 0;
 }
