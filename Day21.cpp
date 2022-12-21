7956a4a0f9c09fdb7ee0ec0991576d71e03fa742
Francois Cayouette <45644908+FCayouette@users.noreply.github.com>
2022-12-21 12:19:34 -0500

    Optimized Day 21 C++20 solution

--- Day21.cpp
+++ Day21.cpp
@@ -10,13 +10,13 @@ enum OP
 struct Monkey
 {
 	i64 value;
-	std::string left, right;
+	int left, right;
 	OP op = None;
 };
-using MonkeyMap = std::map<std::string, Monkey>;
-i64 Evaluate(const std::string& who, const MonkeyMap& map)
+using MonkeyMap = std::vector<Monkey>;
+i64 Evaluate(int who, const MonkeyMap& map)
 {
-	const Monkey& m = map.find(who)->second;
+	const Monkey& m = map[who];
 	if (!m.op)
 		return m.value;
 	i64 l = Evaluate(m.left, map), r = Evaluate(m.right, map);
@@ -29,21 +29,20 @@ i64 Evaluate(const std::string& who, const MonkeyMap& map)
 	}
 }
 
-bool HasMe(const std::string& who, const MonkeyMap& map)
+bool HasMe(int who, const MonkeyMap& map)
 {
-	if (who == "humn") return true;
-	const Monkey& m = map.find(who)->second;
+	if (who == 1) return true;
+	const Monkey& m = map[who];
 	return m.op ? HasMe(m.left, map) || HasMe(m.right, map) : false;
 }
 
-i64 Solve(const std::string& at, i64 equalTo, const MonkeyMap& map)
+i64 Solve(int at, i64 equalTo, const MonkeyMap& map)
 {
-	if (at == "humn") 
-		return equalTo;
-	const Monkey& m = map.find(at)->second;
+	if (at == 1) return equalTo;
+	const Monkey& m = map[at];
 	bool left = HasMe(m.left, map);
 	i64 otherSide = Evaluate(left ? m.right : m.left, map);
-	const std::string& toSolve = left ? m.left : m.right;
+	int toSolve = left ? m.left : m.right;
 	switch (m.op)
 	{
 	case Add: return Solve(toSolve, equalTo - otherSide, map);
@@ -69,32 +68,43 @@ int main(int argc, char* argv[])
 
 	i64 part1 = 0, part2 = 0;
 	std::string line;
-	constexpr std::array<char, 5> Convert= { ' ','+','-','*','/' };
+	constexpr std::array<char, 5> Ops = { 0,'+','-','*','/' };
+	std::vector<std::string> seenMonkeys = { "root", "humn" };
+	MonkeyMap monkeys(2);
 
-	MonkeyMap monkeys;
+	auto GetIndex = [&seenMonkeys, &monkeys](const std::string& s)
+	{
+		if (auto iter = std::find(seenMonkeys.cbegin(), seenMonkeys.cend(), s);
+			iter == seenMonkeys.cend())
+		{
+			int result = seenMonkeys.size();
+			seenMonkeys.push_back(s);
+			monkeys.emplace_back();
+			return result;
+		}
+		else return (int)(iter - seenMonkeys.cbegin());
+	};
+
 	while (std::getline(in, line))
 	{
 		auto p = line.find(':');
-		std::string name = line.substr(0, p);
+		int mIndex = GetIndex(line.substr(0, p));
 		line = line.substr(p + 2);
 		
-		Monkey m;
 		if ((line[0] >= '0' && line[0] <= '9') || line[0] == '-')
-			m.value = stoi(line);
+			monkeys[mIndex].value = stoi(line);
 		else
 		{
 			p = line.find(' ');
-			m.left = line.substr(0, p);
-			char c = line[p + 1];
-			m.op = (OP)(std::find(Convert.cbegin(), Convert.cend(), c) - Convert.cbegin());
-			m.right = line.substr(p + 3);
+			monkeys[mIndex].left = GetIndex(line.substr(0, p));
+			monkeys[mIndex].op = (OP)(std::find(Ops.cbegin(), Ops.cend(), line[p + 1]) - Ops.cbegin());
+			monkeys[mIndex].right = GetIndex(line.substr(p + 3));
 		}
-		monkeys[name] = m;
 	}
-	part1 = Evaluate("root", monkeys);
+	part1 = Evaluate(0, monkeys);
 
-	monkeys["root"].op = Sub;
-	part2 = Solve("root", 0, monkeys);
+	monkeys[0].op = Sub;
+	part2 = Solve(0, 0, monkeys);
 
 	std::cout << std::format("Part 1: {}\nPart 2: {}\n", part1, part2);
 	return 0;
